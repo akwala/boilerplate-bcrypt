d73b5d4ec2c91dbdee22d688e3f464d84e2f39b5
aslam karachiwala <4slam@mythicflow.com>
2021-04-05 22:26:14 -0400

    Only sync (commented out async)

--- server.js
+++ server.js
@@ -12,15 +12,17 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-  /*Store hash in your db*/
-  console.log(hash);
-
-  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-  /*res == true or false*/
-  console.log(res);
-  });
-});
+// bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+//   /*Store hash in your db*/
+//   console.log(hash);
+//   if (err |= undefined) console.log("Error: " + err);
+//
+//   bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+//     /*res == true or false*/
+//     console.log(res);
+//     if (err |= undefined) console.log("Error: " + err);
+//   });
+// });
 
 //END_ASYNC
 
